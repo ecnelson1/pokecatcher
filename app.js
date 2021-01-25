f4c8d2fb1493f36bb68eb4a93ebd696026115515
Eion Nelson <ecnelson1@live.com>
2021-01-25 15:01:07 -0800

    added basic html elements for main and results pages

--- app.js
+++ app.js
@@ -3,3 +3,4 @@
 // initialize state
 
 // set event listeners to update state and DOM
+alert('Welcome To Bons PokeVenture Game! You will have 10 encounters on your quest. each encounter will have three pokemon, but you may only capture one so choose wisely!')
